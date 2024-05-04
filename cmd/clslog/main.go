9086a3cafc58ad7eae032f195eca83a9e32e43ca
CodeXzzz <codexzzz@outlook.com>
2024-05-05 00:08:21 +0800

    [feat][NA]: handle logs with goroutines

--- cmd/clslog/main.go
+++ cmd/clslog/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/json"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
 	"net/http"
+	"sync"
 	"trpc.group/trpc-go/trpc-go"
 	"trpc.group/trpc-go/trpc-go/log"
 	"trpc.group/trpc-go/trpc-go/metrics"
@@ -66,13 +67,14 @@ func NewClsService() *ClsService {
 	}
 }
 
+// SearchClsLog 是搜索CLS日志的方法
 func (s *ClsService) SearchClsLog(req *SearchReq) (*DyeingLogResp, error) {
 	clsReq := cls.NewSearchLogRequest()
 	clsReq.TopicId = common.StringPtr("fill-in-the-topic-id")
 	clsReq.From = common.Int64Ptr(req.BeginTime)
 	clsReq.To = common.Int64Ptr(req.EndTime)
-	// 查询条件，根据传入的Uin、AdPosID、AdID匹配
 	clsReq.Query = common.StringPtr(fmt.Sprintf("uin:%d AND ad_pos_id:%d AND ad_id:%d", req.Uin, req.AdPosID, req.AdID))
+
 	clsResp, err := s.ClsRepo.ClsCli.SearchLog(clsReq)
 	if err != nil {
 		log.Error("SearchClsLog error: %v", err)
@@ -81,37 +83,55 @@ func (s *ClsService) SearchClsLog(req *SearchReq) (*DyeingLogResp, error) {
 	}
 
 	clsLogs := make([]ClsLog, len(clsResp.Response.Results))
+	dyeingLogs := make([]DyeingLog, len(clsResp.Response.Results))
 
-	// 遍历clsResp.Response.Results，获取每个LogInfo中的LogJson字段（即上报的原始日志信息）
-	for _, logInfo := range clsResp.Response.Results {
-		var clsLog ClsLog
-		err := json.Unmarshal([]byte(*logInfo.LogJson), &clsLog)
-		if err != nil {
-			return nil, fmt.Errorf("json.Unmarshal error: %w", err)
-		}
-		clsLogs = append(clsLogs, clsLog)
-	}
+	var wg sync.WaitGroup
+	errorChan := make(chan error)
+	defer close(errorChan)
 
-	dyeingLogs := make([]DyeingLog, len(clsLogs))
-	for _, clsLog := range clsLogs {
-		dyeingLog := DyeingLog{
-			Time:        clsLog.Ftime,
-			Uin:         clsLog.Uin,
-			AdPosID:     clsLog.AdPosID,
-			AdID:        clsLog.AdID,
-			FunnelValue: convert(clsLog.FunnelKey),
-			ExtraInfo:   clsLog.ExtraInfo,
-		}
-		dyeingLogs = append(dyeingLogs, dyeingLog)
+	wg.Add(len(clsResp.Response.Results))
+
+	for i, logInfo := range clsResp.Response.Results {
+		go processLogInfo(i, logInfo, clsLogs, dyeingLogs, errorChan)
 	}
+	wg.Wait()
+
+	err = <-errorChan
+	if err != nil {
+		return nil, fmt.Errorf("json.Unmarshal error: %w", err)
+	}
+	// todo 记得对日志根据层级排序
 	resp := &DyeingLogResp{
 		RequestID: *clsResp.Response.RequestId,
 		Logs:      dyeingLogs,
 	}
-	if len(clsLogs) == 0 {
+
+	if len(dyeingLogs) == 0 {
 		return resp, fmt.Errorf("no logs found")
 	}
+
 	return resp, nil
+}
+
+// processLogInfo 用于处理单个日志信息
+func processLogInfo(i int, logInfo *cls.LogInfo, clsLogs []ClsLog, dyeingLogs []DyeingLog, errorChan chan<- error) {
+	err := json.Unmarshal([]byte(*logInfo.LogJson), &clsLogs[i])
+	if err != nil {
+		select {
+		case errorChan <- err: // 发送错误
+		default: // 不需要重复传入，说明日志里字段就是有问题的
+		}
+		return
+	}
+
+	dyeingLogs[i] = DyeingLog{
+		Time:        clsLogs[i].Ftime,
+		Uin:         clsLogs[i].Uin,
+		AdPosID:     clsLogs[i].AdPosID,
+		AdID:        clsLogs[i].AdID,
+		FunnelValue: convert(clsLogs[i].FunnelKey),
+		ExtraInfo:   clsLogs[i].ExtraInfo,
+	}
 }
 
 // convert 漏斗key转译为可读值
